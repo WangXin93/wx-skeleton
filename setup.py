ecdb05959ae09cde4e2175e7e958c513e071adab
WangX <wangxin19930411@163.com>
2018-03-16 07:36:15 -0700

    Fix package_data name error in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ config = {
     'install_requires':['nose>=1.3.7'],
     'tests_require':['nose'],
     'package_data':{
-        'wx_skeleton':['.gitignore']
+        'wxskeleton':['.gitignore']
     },
     'author':'Wang Xin',
     'author_email':'wangxin19930411@163.com',
