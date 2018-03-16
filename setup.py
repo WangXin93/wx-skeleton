370d65b39c49b97970701eb1593384cf75edaba4
WangX <wangxin19930411@163.com>
2018-03-16 08:10:40 -0700

    Add readme in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ config = {
     'install_requires':['nose>=1.3.7'],
     'tests_require':['nose'],
     'package_data':{
-        'wxskeleton':['.gitignore']
+        'wxskeleton':['.gitignore'],
+        '':['README.md'],
     },
     'author':'Wang Xin',
     'author_email':'wangxin19930411@163.com',
